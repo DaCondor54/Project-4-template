e4ceb4a1495177a52eafe6ff06679c6b70caae05
Enrique Llerena <49108155+DaCondor54@users.noreply.github.com>
2025-12-12 19:56:12 -0500

    Configured Monitoring (#3)

--- backend/Gateway/Program.cs
+++ backend/Gateway/Program.cs
@@ -1,8 +1,10 @@
 using Shared;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.UseHttpClientMetrics();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors();
@@ -16,18 +18,33 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMetricServer();
+app.UseHttpMetrics();
+
 app.UseHttpsRedirection();
 app.UseCors(builder => builder
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader());
 
-app.MapGet("/flem", async (HttpClient httpClient) =>
+app.MapGet("/flem", async (HttpClient httpClient, ILogger<Program> logger) =>
 {
     string host = Environment.GetEnvironmentVariable("RECOMMENDATION_SYSTEM_URL") ?? "resys";
-    var recommendation = await httpClient.GetFromJsonAsync<Recommendation>($"http://{host}:4000/recommendation");
-    string flem = recommendation is null ? "" : $" at {recommendation.Score}";
-    return new Flem($"Yassir === Flem${flem}");
+    try 
+    {
+        var recommendation = await httpClient.GetFromJsonAsync<Recommendation>($"http://{host}:4000/recommendation");
+        string flem = recommendation is null ? "" : $" at {recommendation.Score}";
+
+        logger.LogInformation("Yassir's flem is at {score}", recommendation?.Score);
+
+        return new Flem($"Yassir === Flem${flem}");
+    } 
+    catch(Exception e)
+    {
+        logger.LogWarning("Error: {error}", e.Message);
+        return new Flem("Yassir === Flem ???");
+    }
+
 } ).WithName("GetYassFlem");
 
 app.Run();
